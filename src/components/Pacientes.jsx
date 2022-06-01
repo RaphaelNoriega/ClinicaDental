75821a035bbb8ccf2b9df173bd07804dd3347e1a
RaphaelNoriega <jraphaelnm@gmail.com>
2022-06-01 14:49:14 -0700

    cambios en el componente

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,4 +1,4 @@
- const Pacientes = ({paciente})=>{
+ const Pacientes = ({paciente,setPaciente})=>{
 
     //se le aplica distroctoring
     const {nombre,email,fecha,sintomas} = paciente
@@ -23,6 +23,7 @@
                     <button
                     type="button"
                     className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={()=> setPaciente(paciente)}
                     >Editar</button>
 
                     <button
